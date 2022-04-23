ab19d7e208ad3173e0f1bb28f58b86bc8dadaefa
EC2 Default User <ec2-user@ip-172-31-14-208.ec2.internal>
2022-04-23 11:17:04 +0000

    [update]5-3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,8 +9,7 @@ class FavoritesController < ApplicationController
   
   def destroy
     book = Book.find(params[:book_id])
-    favorite = Favorite.new(book_id: book.id)
-    favorite.user_id = current_user.id
+    favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
     redirect_to book_path(book)
   end
